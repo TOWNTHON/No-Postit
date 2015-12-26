6eaeee27866fc6895a16c3f7bdb5c0b8d0f62cbe
masaki19870528 <masaki19870528@gmail.com>
2015-12-26 20:25:02 +0900

    モック

--- server/NoPost-it/app.js
+++ server/NoPost-it/app.js
@@ -26,4 +26,27 @@ io.sockets.on("connection", function (socket) {
   socket.on("disconnect", function () {
 //    io.sockets.emit("S_to_C_message", {value:"user disconnected"});
   });
+
+  //アップロード処理
+  socket.on('upload', function(data) {
+
+        var fs = require('fs');
+        var writeFile = data.file;
+        var writePath = './public/images/test.jpg';//Σ(ﾟдﾟ) ｴｯ!? 
+
+        var writeStream = fs.createWriteStream(writePath);
+        writeStream.on('drain', function () {} )
+                          .on('error', function (exception) {
+                          //エラー処理
+                               console.log("exception:"+exception);
+                           }) 
+                          .on('close', function () {
+                          //(☞三☞三☞՞ਊ՞)☞三☞三☞書き込み完了時の処理
+                          })  
+                          .on('pipe', function (src) {}); 
+
+        writeStream.write(writeFile,'binary');//バイナリでお願いする
+        writeStreamend();
+        //✌( ◔౪◔)✌<ここに書き込み終了時の処理はかかないで
+    }); 
 });
